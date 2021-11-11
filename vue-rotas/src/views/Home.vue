0cc468771d54bb423eeef15cf6557a4c23a1fd51
Valdeilson Medeiros <valdeilsonms@gmail.com>
2021-11-10 23:48:37 -0300

    v3

---
+++ vue-rotas/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+         <h3 class="font-weight-light">Cadastro de Contatos com Vue Router</h3>   
+    </div>
+</template>
---
+++ vue-rotas/src/views/contatos/Contatos.vue
@@ -0,0 +1,4 @@
+<template>
+ <h3 class="font-weight-light">Contatos</h3>   
+</template>
+  
